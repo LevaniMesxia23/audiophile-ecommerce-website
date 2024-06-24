58c62cc97c7e29c48da6540dcee291ef9bd2d818
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-06-24 21:38:45 +0400

    fix: remove usecontext from Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,9 @@ import CategoryBoxes from "../components/CategoryBoxes";
 import Information from "../components/Information";
 import Footer from "../components/Footer";
 import SeeProducts from "../components/SeeProducts";
-import { useContext } from "react";
-import { MyContext } from "../App";
 
 function Home() {
-  const context = useContext(MyContext)
 
-  if(!context){
-    throw new Error("Header must be used within a MyContext.Provider")
-  }
-  const {show} = context
 
   return (
     <div>
