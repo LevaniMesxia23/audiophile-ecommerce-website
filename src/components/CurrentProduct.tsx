4a63705759cfd35b7b7acd7916c622a1941ed97d
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-04 16:11:41 +0400

    style: add cart buttons on CurrentProduct and formatedPrice

--- src/components/CurrentProduct.tsx
+++ src/components/CurrentProduct.tsx
@@ -6,6 +6,7 @@ function CurrentProduct() {
   const location = useLocation()
   const singleProduct = data.find((item) => item.slug == location.pathname.slice(9))
   console.log(singleProduct?.image.desktop)
+  const formatedPrice = singleProduct?.price.toLocaleString()
   return (
     <>
       <div className=' px-6 pt-4'>
@@ -37,12 +38,22 @@ function CurrentProduct() {
           ) : null}
           <h2 className=' text-[1.75rem] mt-6 font-bold tracking-[0.625rem] uppercase '>{singleProduct?.name}</h2>
           <p className=' text-[0.9375rem] landing-[1.5625rem] my-6 text-black/50'>{singleProduct?.description}</p>
-          <p className=' mb-[1.94rem] tracking-[0.08038rem] font-bold uppercase text-[1.125rem]'>$ {singleProduct?.price}</p>
+          <p className=' mb-[1.94rem] tracking-[0.08038rem] font-bold uppercase text-[1.125rem]'>$ {formatedPrice}</p>
         </div>
       </div>
+
+      <div className=' flex items-center w-full justify-center gap-4'>
+        <div className=' py-[0.94rem] px-[0.97rem] min-w-[7.5rem] bg-[#F1F1F1] flex items-center justify-center gap-5 h-12'>
+          <span className=' text-black/25'>-</span>
+          <span>{1}</span>
+          <span className=' text-black/25'>+</span>
+        </div>
+        <button className="w-full min-w-[10rem] h-[3rem] bg-[#D87D4A] text-white text-[0.8125rem]">ADD TO CART</button>
+      </div>
+
       <div>
         <div>
-          <h2>FEATURES</h2>
+          <h2 className=' mt-[5.5rem] mb-6 text-[1.5rem] font-bold leading-[2.25rem] tracking-[0.05356rem]'>FEATURES</h2>
           <p>{singleProduct?.features}</p>
         </div>
 
