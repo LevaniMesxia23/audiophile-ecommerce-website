ba9b2bd5a0757a724a896b62a753761333d19b7e
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-08 14:17:57 +0400

    style: create shipping info styles

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -30,7 +30,37 @@ function Checkout() {
         </div>
 
       </div>
+
+      <div className=" p-6 flex flex-col items-start justify-center">
+        <span className=" text-[#D87D4A] text-[0.8125rem] leading-[1.5625rem] tracking-[0.05806rem] font-bold uppercase mb-4">shipping info</span>
+
+        <div className=" flex flex-col gap-4 w-full">
+        <div className=" gap-[0.56rem] flex flex-col">
+          <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">Your Address</span>
+          <input className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]" placeholder="1137 Williams Avenue" type="text" />
+        </div>
+
+        <div className=" gap-[0.56rem] flex flex-col">
+          <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">ZIP Code</span>
+          <input className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]" placeholder="10001" type="text" />
+        </div>
+
+        <div className=" gap-[0.56rem] flex flex-col">
+          <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">City</span>
+          <input className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]" placeholder="New York" type="text" />
+        </div>
+
+        <div className=" gap-[0.56rem] flex flex-col">
+          <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">Country</span>
+          <input className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]" placeholder="United States" type="text" />
+        </div>
+
+        </div>
+
       </div>
+      </div>
+
+      
     </div>
   )
 }
