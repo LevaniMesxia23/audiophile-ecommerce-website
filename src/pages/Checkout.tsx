a3994508de77113dca582d5dc75bade70284bc10
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-10 14:05:33 +0400

    feat: add payment details functionality

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import loginSchema from "../loginSchema"
 import InputMask from "react-input-mask"
+import { useState } from "react"
+import Shape from "../../public/Shape.svg"
 function Checkout() {
   const {register, handleSubmit, formState: {errors},} = useForm({
     resolver: yupResolver(loginSchema)
@@ -12,6 +14,7 @@ function Checkout() {
     
   }
   const navigate = useNavigate()
+  const [showCash, setShowCash] = useState(true)
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='px-6 pt-4 bg-[#FAFAFA]'>
       <span className='text-[0.9375rem] opacity-50 leading-[1.5625rem]' onClick={() => navigate(-1)}>Go Back</span>
@@ -87,18 +90,18 @@ function Checkout() {
         <div className=" flex flex-col gap-4 w-full">
         <div className=" gap-[0.56rem] flex flex-col">
           <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">Payment Method</span>
-          <div className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 flex items-center gap-4">
+          <div onClick={() => setShowCash(!showCash)} className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 flex items-center gap-4">
             <div className=" w-5 h-5 border-[0.0625rem] border-[#CFCFCF] rounded-[50%] flex items-center justify-center">
-              <div className="w-[0.625rem] h-[0.625rem] border-none border-[#CFCFCF] rounded-[50%] bg-[#D87D4A]"></div>
+            {!showCash && <div className="w-[0.625rem] h-[0.625rem] border-none border-[#CFCFCF] rounded-[50%] bg-[#D87D4A]"></div>}
             </div>
               <span>e-Money</span>
           </div>
         </div>
 
         <div className=" gap-[0.56rem] flex flex-col">
-          <div className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 flex items-center gap-4">
+          <div onClick={() => setShowCash(!showCash)} className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 flex items-center gap-4">
             <div className=" w-5 h-5 border-[0.0625rem] border-[#CFCFCF] rounded-[50%] flex items-center justify-center">
-              <div className="w-[0.625rem] h-[0.625rem] border-none border-[#CFCFCF] rounded-[50%] bg-[#D87D4A]"></div>
+              {showCash && <div className="w-[0.625rem] h-[0.625rem] border-none border-[#CFCFCF] rounded-[50%] bg-[#D87D4A]"></div>}
             </div>
               <span>Cash on Delivery</span>
           </div>
@@ -106,7 +109,7 @@ function Checkout() {
 
         </div>
 
-        <div className=" w-full">
+        {!showCash && <div className=" w-full">
 
         <div className=" gap-[0.56rem] flex flex-col mt-8">
           <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">e-Money Number</span>
@@ -116,7 +119,11 @@ function Checkout() {
           <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">e-Money PIN</span>
           <input className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]" placeholder="6891" type="text" />
         </div>
-        </div>
+        </div>}
+        {showCash && <div className=" flex gap-8 items-center mt-[1.88rem]">
+          <img src={Shape} alt="" />
+        <span className=" text-[0.9375rem] leading-[1.5625rem] text-black/50">The ‘Cash on Delivery’ option enables you to pay in cash when our delivery courier arrives at your residence. Just make sure your address is correct so that your order will not be cancelled.</span>
+        </div>}
       </div>
       </div>
 
