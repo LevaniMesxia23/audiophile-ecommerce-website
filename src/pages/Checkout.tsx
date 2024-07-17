d0bbf0c5e21a00086aac39a832a38238bac33bc7
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-17 20:29:59 +0400

    style: add Checkout tablet responsive

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -53,19 +53,21 @@ function Checkout() {
         <span className=" text-[#D87D4A] text-[0.8125rem] leading-[1.5625rem] tracking-[0.05806rem] font-bold uppercase mb-4">Billing details</span>
 
         <div className=" flex flex-col gap-4 w-full">
-        <div className="gap-[0.56rem] flex flex-col">
-          <span className={`${getErrorText(errors.name)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>Name</span>
-          <input {...register("name")} className={`border-[0.0625rem] ${getInputClassName(errors.name)} w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="Alexei Ward" type="text" />
-          {errors.name && <p className="text-[#CD2C2C]">{errors.name.message}</p>}
-        </div>
+          <div className=" md:w-full md:flex md:gap-4">
+            <div className="gap-[0.56rem] flex flex-col md:w-full">
+              <span className={`${getErrorText(errors.name)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>Name</span>
+              <input {...register("name")} className={`border-[0.0625rem] ${getInputClassName(errors.name)} w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="Alexei Ward" type="text" />
+              {errors.name && <p className="text-[#CD2C2C]">{errors.name.message}</p>}
+            </div>
 
-        <div className=" gap-[0.56rem] flex flex-col">
-          <span className={`${getErrorText(errors.email)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>Email Address</span>
-          <input {...register("email")} className={`${getInputClassName(errors.email)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="alexei@mail.com" type="text" />
-          {errors.email && <p className="text-[#CD2C2C]">{errors.email.message}</p>}
-        </div>
+            <div className=" gap-[0.56rem] flex flex-col md:w-full">
+              <span className={`${getErrorText(errors.email)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>Email Address</span>
+              <input {...register("email")} className={`${getInputClassName(errors.email)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="alexei@mail.com" type="text" />
+              {errors.email && <p className="text-[#CD2C2C]">{errors.email.message}</p>}
+            </div>
+          </div>
 
-        <div className=" gap-[0.56rem] flex flex-col">
+        <div className=" gap-[0.56rem] flex flex-col md:w-[50%]">
           <span className={` ${getErrorText(errors.phoneNumber)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>Phone Number</span>
           <Controller
                 name="phoneNumber"
@@ -95,18 +97,19 @@ function Checkout() {
           <input {...register("address")} className={` ${getInputClassName(errors.address)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="1137 Williams Avenue" type="text" />
           {errors.address && <p className="text-[#CD2C2C]">{errors.address.message}</p>}
         </div>
-
-        <div className=" gap-[0.56rem] flex flex-col">
+      <div className=" md:w-full md:flex md:gap-4">
+        <div className=" gap-[0.56rem] flex flex-col md:w-full">
           <span className={` ${getErrorText(errors.zipCode)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>ZIP Code</span>
           <input {...register("zipCode")} className={` ${getInputClassName(errors.zipCode)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="10001" type="number" />
           {errors.zipCode && <p className="text-[#CD2C2C]">{errors.zipCode.message}</p>}
         </div>
 
-        <div className=" gap-[0.56rem] flex flex-col">
+        <div className=" gap-[0.56rem] flex flex-col md:w-full">
           <span className={` ${getErrorText(errors.city)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>City</span>
           <input {...register("city")} className={` ${getInputClassName(errors.city)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="New York" type="text" />
           {errors.city && <p className="text-[#CD2C2C]">{errors.city.message}</p>}
         </div>
+      </div>
 
         <div className=" gap-[0.56rem] flex flex-col">
           <span className={`${getErrorText(errors.country)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>Country</span>
@@ -119,9 +122,9 @@ function Checkout() {
         <div className=" p-6 flex flex-col items-start justify-center">
         <span className=" text-[#D87D4A] text-[0.8125rem] leading-[1.5625rem] tracking-[0.05806rem] font-bold uppercase mb-4">payment details</span>
 
-        <div className=" flex flex-col gap-4 w-full">
-        <div className=" gap-[0.56rem] flex flex-col">
           <span className=" text-[0.75rem] font-bold -tracking-[0.01563rem]">Payment Method</span>
+        <div className=" flex flex-col gap-4 w-full md:items-end">
+        <div className=" gap-[0.56rem] flex flex-col md:w-[50%]">
           <div onClick={() => setShowCash(false)} className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 flex items-center gap-4">
             <div className=" w-5 h-5 border-[0.0625rem] border-[#CFCFCF] rounded-[50%] flex items-center justify-center">
             {!showCash && <div className="w-[0.625rem] h-[0.625rem] border-none border-[#CFCFCF] rounded-[50%] bg-[#D87D4A]"></div>}
@@ -130,7 +133,7 @@ function Checkout() {
           </div>
         </div>
 
-        <div className=" gap-[0.56rem] flex flex-col">
+        <div className=" gap-[0.56rem] flex flex-col md:w-[50%]">
           <div onClick={() => setShowCash(true)} className=" border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 flex items-center gap-4">
             <div className=" w-5 h-5 border-[0.0625rem] border-[#CFCFCF] rounded-[50%] flex items-center justify-center">
               {showCash && <div className="w-[0.625rem] h-[0.625rem] border-none border-[#CFCFCF] rounded-[50%] bg-[#D87D4A]"></div>}
@@ -140,13 +143,14 @@ function Checkout() {
         </div>
         </div>
 
-        {!showCash && <div className=" w-full">
-        <div className=" gap-[0.56rem] flex flex-col mt-8">
+        {!showCash &&
+         <div className=" w-full md:w-full md:flex md:gap-4">
+          <div className=" gap-[0.56rem] flex flex-col mt-8 md:w-[50%]">
           <span className={` ${getErrorText(errors.emoneyNum)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>e-Money Number</span>
           <input {...register("emoneyNum")} className={` ${getInputClassName(errors.emoneyNum)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="238521993" type="number" />
           {errors.emoneyNum && <p className="text-[#CD2C2C]">{errors.emoneyNum.message}</p>}
         </div>
-        <div className=" gap-[0.56rem] flex flex-col mt-6">
+        <div className=" gap-[0.56rem] flex flex-col mt-8 md:w-[50%]">
           <span className={` ${getErrorText(errors.emoneyPin)} text-[0.75rem] font-bold -tracking-[0.01563rem]`}>e-Money PIN</span>
           <input {...register("emoneyPin")} className={` ${getInputClassName(errors.emoneyPin)} border-[0.0625rem] border-[#CFCFCF] w-full h-[3.5rem] rounded-[0.5rem] pl-6 placeholder:font-bold placeholder:text-[0.875rem] placeholder:-tracking-[0.01563rem]`} placeholder="6891" type="number" />
           {errors.emoneyPin && <p className="text-[#CD2C2C]">{errors.emoneyPin.message}</p>}
