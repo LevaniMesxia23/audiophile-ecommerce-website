2f4a89ca845a17e54c6b0917e348df67cb148175
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-18 23:05:58 +0400

    style: add keyframes in twconfig

--- src/components/CategoryBoxes.tsx
+++ src/components/CategoryBoxes.tsx
@@ -6,94 +6,59 @@ import { Link } from "react-router-dom";
 import { useEffect, useContext } from "react";
 import { MyContext } from "../App";
 
-function CategoryBoxes({animation} : {animation : boolean} ) {
-  const context = useContext<MyContextType | null>(MyContext)
-  if(!context){
-    throw new Error("Header must be used within a MyContext.Provider")
+function CategoryBoxes({ animation }: { animation: boolean }) {
+  const context = useContext(MyContext);
+  if (!context) {
+    throw new Error("CategoryBoxes must be used within a MyContext.Provider");
   }
-  const { show, setShow } = context
+  const { show, setShow } = context;
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   const handleClick = () => {
-    setShow(false)
-  }
+    setShow(false);
+  };
+
   return (
-    <>
-    <div className={`w-full ${show && animation ? 'animate-slideRight' : 'animate-slideLeft'}`}>
-    <div className="bg-white pb-10 flex flex-col md:flex-row md:justify-center md:gap-[0.62rem]">
-      <div className=" flex flex-col justify-center items-center">
-        <div className="w-[23.4375rem] md:w-[13.9375rem] mt-20 relative flex justify-center">
-          <div className=" w-full bg-[#F1F1F1] rounded-[0.5rem] max-w-[20.4375rem]">
-            <div className=" flex items-center justify-center flex-col gap-[1.06rem]">
-              <span className=" mt-[5.5rem]">HEADPHONES</span>
-              <div className=" flex items-center gap-[0.83rem] mb-[1.57rem]">
-                <Link to={"/headphones"} onClick={handleClick}><span className=" text-black opacity-[0.5] cursor-pointer">SHOP</span></Link>
-                <img
-                  src={ArrowRight}
-                  alt=""
-                  className=" w-[0.3125rem] h-[0.625rem]"
-                />
+    <div className={`w-full ${show && animation ? 'animate-slideRight' : ''}`}>
+      <div className="bg-white pb-10 flex flex-col md:flex-row md:justify-center md:gap-[0.62rem]">
+        {[{
+          name: "HEADPHONES",
+          link: "/headphones",
+          image: HeadphonesImage,
+          imgStyle: "left-[38%] md:left-[32%]"
+        }, {
+          name: "SPEAKERS",
+          link: "/speakers",
+          image: SpeakersImage,
+          imgStyle: "left-[37%] md:left-[30%]"
+        }, {
+          name: "EARPHONES",
+          link: "/earphones",
+          image: EarphonesImage,
+          imgStyle: "left-[35%] md:left-[25%]"
+        }].map(({ name, link, image, imgStyle }, index) => (
+          <div key={index} className="flex flex-col justify-center items-center">
+            <div className="w-[23.4375rem] md:w-[13.9375rem] mt-20 relative flex justify-center">
+              <div className="w-full bg-[#F1F1F1] rounded-[0.5rem] max-w-[20.4375rem]">
+                <div className="flex items-center justify-center flex-col gap-[1.06rem]">
+                  <span className="mt-[5.5rem]">{name}</span>
+                  <div className="flex items-center gap-[0.83rem] mb-[1.57rem]">
+                    <Link to={link} onClick={handleClick}>
+                      <span className="text-black opacity-[0.5] cursor-pointer">SHOP</span>
+                    </Link>
+                    <img src={ArrowRight} alt="" className="w-[0.3125rem] h-[0.625rem]" />
+                  </div>
+                </div>
               </div>
+              <img src={image} alt={name} className={`absolute -top-[53px] ${imgStyle}`} />
             </div>
           </div>
-          <img
-            src={HeadphonesImage}
-            alt=""
-            className=" absolute -top-[53px] left-[38%] md:left-[32%]"
-          />
-        </div>
-      </div>
-
-      <div className=" flex flex-col justify-center items-center">
-        <div className="w-[23.4375rem] md:w-[13.9375rem] mt-20 relative flex justify-center">
-          <div className=" w-full bg-[#F1F1F1] rounded-[0.5rem] max-w-[20.4375rem]">
-            <div className=" flex items-center justify-center flex-col gap-[1.06rem]">
-              <span className=" mt-[5.5rem]">SPEAKERS</span>
-              <div className=" flex items-center gap-[0.83rem] mb-[1.57rem]">
-                <Link to={"/speakers"} onClick={handleClick}><span className=" text-black opacity-[0.5] cursor-pointer">SHOP</span></Link>
-                <img
-                  src={ArrowRight}
-                  alt=""
-                  className=" w-[0.3125rem] h-[0.625rem]"
-                />
-              </div>
-            </div>
-          </div>
-          <img
-            src={SpeakersImage}
-            alt=""
-            className=" absolute -top-[53px] left-[37%] md:left-[30%]"
-          />
-        </div>
-      </div>
-
-      <div className=" flex flex-col justify-center items-center">
-        <div className="w-[23.4375rem] md:w-[13.9375rem] mt-20 relative flex justify-center">
-          <div className=" w-full bg-[#F1F1F1] rounded-[0.5rem] max-w-[20.4375rem]">
-            <div className=" flex items-center justify-center flex-col gap-[1.06rem]">
-              <span className=" mt-[5.5rem]">EARPHONES</span>
-              <div className=" flex items-center gap-[0.83rem] mb-[1.57rem]">
-                <Link to={"/earphones"} onClick={handleClick}><span className=" text-black opacity-[0.5] cursor-pointer">SHOP</span></Link>
-                <img
-                  src={ArrowRight}
-                  alt=""
-                  className=" w-[0.3125rem] h-[0.625rem]"
-                />
-              </div>
-            </div>
-          </div>
-          <img
-            src={EarphonesImage}
-            alt=""
-            className=" absolute -top-[53px] left-[35%] md:left-[25%]"
-          />
-        </div>
+        ))}
       </div>
     </div>
-    </div>
-    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,19 +25,33 @@ export default {
         '100%': { opacity: 0, transform: 'translateY(-40px)' },
       },
       slideRight: {
-        '0%' : {opacity: 0, transform: 'transtlateX(-40px)'},
+        '0%' : {opacity: 0, transform: 'transtlateX(-100px)'},
         '100%': { opacity: 1, transform: 'translateX(0)' },
       },
       slideLeft: {
         '0%' : {opacity: 1, transform: 'transtlateX(0)'},
-        '100%': { opacity: 0, transform: 'translateX(-40px)' },
-      }
+        '100%': { opacity: 0, transform: 'translateX(-100px)' },
+      },
+      titleSlideLeft: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0)' },
+      },
+      titleSlideRight: {
+        '0%': { transform: 'translateX(0)' },
+        '100%': { transform: 'translateX(100%)' },
+      },
+      titleSlideMobile: {
+        '0%': { transform: 'translateY(100%)' },
+        '100%': { transform: 'translateY(0)' },
+      },
     },
     animation: {
       slideIn: 'slideIn 0.3s ease-out',
       slideOut: 'slideOut 0.3s ease-out',
-      slideRight: 'slideRight 0.3s ease-out',
-      slideLeft: 'slideLeft 0.3s ease-out',
+      slideRight: 'slideRight 1.5s ease-out',
+      slideLeft: 'slideLeft 1.5s ease-out',
+      titleSlideRight: 'titleSlideRight 0.5s ease-out',
+      titleSlideLeft: 'titleSlideLeft 0.5s ease-out',
     },
   },
   plugins: [],
